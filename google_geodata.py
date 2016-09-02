74acf644fc74c61cd67462aba9b51ae3eafb2660
yennanliu <f339339@gmail.com>
2016-09-02 15:46:55 +0800

    Update google_geodata.py

--- google_geodata.py
+++ google_geodata.py
@@ -5,21 +5,6 @@ ref :  https://developers.google.com/maps/documentation/geocoding/get-api-key
 ref :  https://developers.google.com/maps/documentation/geocoding/start
 '''
 
-from IPython.display import display, HTML
-import pandas as pd, numpy as np 
-%pylab inline 
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-import seaborn as sns
-import time
-import datetime as dt   
-import os
-from pandas import ExcelWriter
-#import goslate
-import requests
-
-
-
 
 
 df = pd.read_excel('0701.xlsx')
@@ -27,34 +12,30 @@ df.columns
 dd=df[['地址.1','區域']]
 dd=dd.dropna()
 
-# save as chinese 
 dd.columns=['address','group']
 dd.to_csv('test0701.csv',encoding='utf-8')
 
 
-
-# google API part 
-
-google_api = your_google_api_key
+google_api = 'AIzaSyBDrQZBasCaGP_j22GGopF-S4EPnINvmh8'
 address='taipei 101'
-url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address,google_api)
-
-r = requests.get(url)
-results = r.json()
-results['results'][0]['geometry']['bounds']['northeast']
-
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=25.0330,121.5654&radius=50000&keyword={}&key={}'.format(address,google_api)
 
 
 def geo(x):
+	lon_lat=[[] for k in range(2)]
+
 	for i,v in enumerate(x):
-		address=v
-		url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address,google_api)
-		print (i,v)
+		address=v 
+		url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=25.0330,121.5654&radius=50000&keyword={}&key={}'.format(address,google_api)
+		r = requests.get(url)
 		results = r.json()
-		#return results['results'][0]['geometry']['bounds']['northeast']
+		lon_lat[0].append(results['results'][0]['geometry']['location']['lng'])
+		lon_lat[1].append(results['results'][0]['geometry']['location']['lat'])
+	geo_data=pd.DataFrame(lon_lat).T 
+	return geo_data
 
 
-
-
-
-dd['lon_lat'] =dd['address'].apply(lambda x : geo(x))
+		
+data=geo(dd['address'])
+data.columns=['lat','lon']
+data=pd.merge(dd,data, left_index=True,right_index=True)
