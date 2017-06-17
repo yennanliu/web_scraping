78bb69040fb2fe2a2194c018d757375639a0c0f3
yennanliue <f339339@gmail.com>
2017-06-17 17:55:50 +0800

    add env install script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+# set up env
+
+read -p "Please set your env name: " env_name
+
+echo 'your env name :' $env_name
+
+
+echo 'creating env....'
+
+conda create --name $env_name python=3 &&
+
+
+# install library 
+
+echo 'start install env.... '
+
+source activate $env_name &&  pip install pandas urllib beautifulsoup4
+
+echo 'all env library installed successfully ! '
+
+
+
+
+# if want to delete env 
+# conda remove --name your_env_name --all
