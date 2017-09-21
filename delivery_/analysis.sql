b285a4ea2d707b5e29514def54dc05307dd5962a
yennanliue <f339339@gmail.com>
2017-09-21 18:01:50 +0100

    backup test sql

---
+++ delivery_/analysis.sql
@@ -0,0 +1,43 @@
+# http://www.codedata.com.tw/database/mysql-tutorial-13-stored-routines/
+
+# https://www.a2hosting.co.uk/kb/developer-corner/mysql/mysql-stored-functions-and-procedures
+
+
+
+
+#  stored function
+DELIMITER $$
+CREATE FUNCTION plus(temp_max FLOAT, temp_min FLOAT) RETURNS DECIMAL(9,2)
+BEGIN
+  DECLARE tem__ DECIMAL(9,2);
+  SET tem__ = temp_max + temp_min;
+  RETURN tem__;
+END$$
+DELIMITER ;
+
+
+# sql 
+SELECT *, plus(temp_max,temp_min) AS tem_test FROM weather_data;
+
+
+#  stored function
+DELIMITER $$
+CREATE PROCEDURE procedureTest()
+BEGIN
+  SELECT CET FROM weather_data;
+END$$
+DELIMITER ;
+
+# execute 
+CALL procedureTest() \G
+
+
+
+
+
+
+
+
+
+
+
