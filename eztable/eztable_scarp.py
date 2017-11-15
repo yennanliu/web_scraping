c7129b4aa3751a356e0a27cb6dfbedfef6aefb10
yennanliue <f339339@gmail.com>
2017-11-15 21:43:48 +0000

    add code comments

--- eztable/eztable_scarp.py
+++ eztable/eztable_scarp.py
@@ -5,18 +5,20 @@ from selenium import webdriver
 import time, re
 from bs4 import BeautifulSoup
 
-
-url = 'https://tw.eztable.com/search?q=%E6%97%A5%E6%9C%AC%E6%96%99%E7%90%86'
-
+# grab "steak restaurant "
+url = 'https://tw.eztable.com/search?q=%E7%89%9B%E6%8E%92'
+# set up selenium driver (via firefox)
 driver = webdriver.Firefox()
 driver.implicitly_wait(3)
 driver.get(url)
-
-for i in range(1,10):
+# set grab 50 pages 
+for i in range(1,50):
 	driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+	### wait 1 sec tull JS/ajax load the all contents 
 	time.sleep(1)
 
-
+# analysis web page generate by JS via BeautifulSoup
 soup = BeautifulSoup(driver.page_source, "html5lib")
 for block in soup.find_all('h5'):
+	# print restaurant name 
 	print (block.text)
