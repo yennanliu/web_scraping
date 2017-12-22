ca4fa29e8cf66da8c9bd6049ec17a58039e55078
yennanliue <f339339@gmail.com>
2017-12-22 11:15:35 +0000

    finish 1st simple scraper and return data as dataframe

--- blu_move/blu_scrape_V1.py
+++ blu_move/blu_scrape_V1.py
@@ -10,6 +10,10 @@ import sys ,re,time
 
 url="https://app.bluemove.es/api/public/locations/list?cityId=100&accountId=1"
 
+def extract_data():
+	pass 
+
+
 def main():
 	print (url)
 	opener=urllib.request.build_opener()
@@ -23,10 +27,37 @@ def main():
 
 
 
+def main_():
+	print (url)
+	opener=urllib.request.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	page = opener.open(url)
+	soup = BeautifulSoup(page)
+	geo_data =  dict(json.loads(soup.text))
+	geo_data_ = geo_data['data']['locations']
+	print (geo_data_)
+	print ('length of data :',len(geo_data_) )
+
+	# transfer to  dataframe 
+	output = [[] for k in range(5)]
+	for count in range(len(geo_data['data']['locations'])):
+		scraped_data = geo_data['data']['locations'][count]
+		for k,j in enumerate(scraped_data['Location']['vehicles']):
+		    #print (k)
+			output[0].append(scraped_data['Location']['vehicles'][k]['id'])
+			output[1].append(scraped_data['Location']['vehicles'][k]['gpslat'])
+			output[2].append(scraped_data['Location']['vehicles'][k]['gpslong'])
+			output[3].append(scraped_data['Location']['vehicles'][k]['gps_timestamp'])
+			output[4].append(scraped_data['Location']['vehicles'][k]['status'])
+	df_ = pd.DataFrame(output).T
+	df_.columns = [['id','gpslat','gpslong','gps_timestamp','status']]
+	print (df_) 
+
+
 
 
 if __name__ == '__main__':
-	main()
+	main_()
 
 
 
