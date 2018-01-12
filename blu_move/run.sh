f434617ce1b59cf66dae3c6793a7b3b38c4e7f83
yennanliue <f339339@gmail.com>
2018-01-12 11:00:52 +0000

    make shell runs OK for different local envs

--- blu_move/run.sh
+++ blu_move/run.sh
@@ -4,7 +4,7 @@
 echo "scraping via shell ...."
 bash blu_scrape_V2.sh 
 echo "prepare data python ...."
-#/anaconda/envs/g_dash/bin/python blu_scrape_V2.py
-source activate zip_dev && python blu_scrape_V2.py
+# 2 python script run way in case of different local dev envs
+source activate zip_dev && python blu_scrape_V2.py ||  /anaconda/envs/g_dash/bin/python blu_scrape_V2.py
 #echo "clean file..."
 
