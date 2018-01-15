8585dbe10b701aaa595be53f3aa6858014a8bf35
yennanliue <f339339@gmail.com>
2018-01-15 07:00:19 +0000

    add utilization sql

--- blu_move/analysis.sql
+++ blu_move/analysis.sql
@@ -1,4 +1,5 @@
 
+-- 1. Duration period 
 -- get duration period (minute) per booking per car 
 
 SELECT id,
@@ -15,3 +16,29 @@ FROM
      AND start_reservation IS NOT NULL ) sub
 ORDER BY id,
          end_reservation
+
+
+-- 1. utilization  
+-- get utilization per day 
+
+
+
+WITH booking AS
+  (SELECT date(start_reservation) AS date,
+          count(DISTINCT id) AS booked_car
+   FROM <table_name>
+   GROUP BY 1),
+     all_ AS
+  (SELECT date(date_of_insert) AS date,
+          count(DISTINCT id) AS all_car
+   FROM <table_name>
+   GROUP BY 1)
+SELECT booking.*,
+       all_.all_car,
+       booking.booked_car::NUMERIC/all_.all_car::NUMERIC AS utilization
+FROM booking
+INNER JOIN all_ ON booking.date = all_.date
+ORDER BY booking.date
+
+
+
