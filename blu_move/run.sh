12579f14feb47cb126bc80d4ad2b84496c0e9ce9
yennanliue <f339339@gmail.com>
2018-01-15 09:58:10 +0000

    modify make scrapper work OK on cloud

--- blu_move/run.sh
+++ blu_move/run.sh
@@ -5,7 +5,7 @@ echo "scraping via shell ...."
 bash blu_scrape_V2.sh 
 echo "prepare data python ...."
 # 2 python script run way in case of different local dev envs
-source activate zip_dev && python blu_scrape_V2.py ||  /Users/yennanliu/anaconda3/envs/ds_dash/bin/python blu_scrape_V2.py
+source activate zip_dev && python blu_scrape_V2.py ||  /Users/yennanliu/anaconda3/envs/ds_dash/bin/python blu_scrape_V2.py || python /home/ubuntu/yen_dev/blu_move/blu_scrape_V2.py
 
 
 #echo "clean file..."
