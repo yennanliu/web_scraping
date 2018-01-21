ea4d9a9e4d7f0ae3a4387a0419f113e17664fe8b
yennanliue <f339339@gmail.com>
2018-01-21 23:31:05 +0000

    fix duration for using across 1 day cases

--- blu_move/analysis.sql
+++ blu_move/analysis.sql
@@ -66,13 +66,20 @@ ORDER BY booking.date
 
 
 -- 4. dev utilization   (hour)
-# hour of using / 24 hour for each car 
+### fix the "duration across day problem"
+### e.g. start : 2017-01-01 23:00, end : 2017-01-02 07:00 
+# hour of using / 24 hour for each car  (V1)
 
 
-WITH get_last_log AS
-  (SELECT b.start_reservation AS start_reservation_,
-          b.end_reservation,
-          b.*,
+WITH dates AS
+  ( SELECT DISTINCT generate_series(min(b.start_reservation::date) OVER (PARTITION BY b.id)::TIMESTAMP, now()::date - '1 day'::interval, '1 day'::interval)::date AS date,
+                    b.id,
+                    24 AS capacity_hours,
+                    1 AS capacity_days
+   FROM rw.blue_move b
+   WHERE date(b.start_reservation) >= '2018-01-12' ),
+     get_last_log AS
+  (SELECT b.*,
           ROW_NUMBER() OVER (PARTITION BY id,
                                           date(date_of_insert)
                              ORDER BY date(date_of_insert),
@@ -80,15 +87,44 @@ WITH get_last_log AS
                             ROW_NUMBER() OVER (PARTITION BY id,
                                                             start_reservation
                                                ORDER BY date_of_insert DESC) AS row_id_
-   FROM <table_name> b
+   FROM rw.blue_move b
    WHERE start_reservation IS NOT NULL
      AND end_reservation IS NOT NULL )
-SELECT *
-FROM get_last_log
+SELECT d_1.date,
+       last_log.id,
+       last_log.start_reservation,
+       last_log.end_reservation,
+       CASE
+           WHEN last_log.start_reservation < d_1.date
+                AND last_log.end_reservation::date > d_1.date THEN 24::double precision
+           WHEN last_log.start_reservation < d_1.date THEN date_part('hour'::text, last_log.end_reservation) + date_part('minute'::text, last_log.end_reservation) / 60::double precision
+           WHEN last_log.start_reservation::date = d_1.date
+                AND last_log.end_reservation::date > d_1.date THEN date_part('epoch'::text, d_1.date + '1 day'::interval - last_log.start_reservation) / 3600::double precision
+           WHEN last_log.start_reservation::date = d_1.date
+                AND last_log.end_reservation::date = d_1.date THEN date_part('epoch'::text, last_log.end_reservation - last_log.start_reservation) / 3600::double precision
+           ELSE 0::double precision
+       END AS service_hours
+FROM get_last_log last_log
+RIGHT JOIN dates d_1 ON (last_log.start_reservation::date <= d_1.date
+                         AND last_log.end_reservation::date >= d_1.date
+                         OR last_log.start_reservation::date = d_1.date)
+AND d_1.id::text = last_log.id::text
 WHERE row_id = 1
   AND row_id_ = 1
 ORDER BY id,
-         start_reservation_,
-         row_id_
+         start_reservation, date
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
