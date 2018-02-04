a523b40c607d5d12d6011918ead9d859ba479cb6
yennanliue <f339339@gmail.com>
2018-02-04 10:53:17 +0000

    concat scrape row as output dataframe and update data filter

--- Brussels_weather/br_weather_scrapper_V1.py
+++ Brussels_weather/br_weather_scrapper_V1.py
@@ -4,43 +4,55 @@ import urllib, json
 from bs4 import BeautifulSoup
 
 
-def main():
-	url_new = 'https://www.wunderground.com/history/airport/EBFS/2017/11/30/DailyHistory.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo='
-	print (url_new)
 
 
-	# query the page 
-	opener=urllib.request.build_opener()
-	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-	page = opener.open(url_new)
-	soup = BeautifulSoup(page)
-	trs = soup.find_all('td', attrs={'class': 'indent'})
-	col=[]
-	val=[]
-	for tr in trs:
-		tds = tr.find_next_siblings("td") # you get list
-		print (tr.text )
-		col.append(tr.text)
-		print (tds[0].text)
-		val.append(tds[0].text.strip('\n')
-			.replace('\xa0','')
-			.replace('°C','')
-			.replace('mm','')
-			.replace('hPa','')
-			.replace('km/h\n ()','')
-			.replace('km/h','')
-			.replace('kilometers',''))
+def main_():
+    output=pd.DataFrame([])
+    
+    for k in range(1,10):    
+        #url_new = 'https://www.wunderground.com/history/airport/EBFS/2017/11/30/DailyHistory.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo='
+        url_new = 'https://www.wunderground.com/history/airport/EBFS/2017/11/{}/DailyHistory.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo='.format(k)
+        print (url_new)
+        
+        # query the page 
+        opener=urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        page = opener.open(url_new)
+        soup = BeautifulSoup(page)
+        trs = soup.find_all('td', attrs={'class': 'indent'})
+        col=[]
+        val=[]
+        for tr in trs:
+            tds = tr.find_next_siblings("td") # you get list
+            print (tr.text )
+            col.append(tr.text)
+            print (tds[0].text)
+            val.append(tds[0].text.strip('\n')
+                .replace('\xa0','')
+                .replace('°C','')
+                .replace('mm','')
+                .replace('hPa','')
+                .replace('km/h\n ()','')
+                .replace('km/h','')
+                .replace('kilometers','')
+                .replace('\n\t', '')
+                .replace('\t', '')
+                .replace('\n', ''))
 
-	df = pd.DataFrame({'col':col,'val':val}).set_index('col').T.reset_index()
-	del df['index']
-	print (df)
-	return df 
+        df = pd.DataFrame({'col':col,'val':val}).set_index('col').T.reset_index()
+        del df['index']
+        #print ('df :', df)
+        ### update output dataframe 
+        output = output.append(df)
+    output = output.reset_index()
+    print (output)
+    return output 
 
 
 
 
 if __name__ == '__main__':
-	main()
+	main_()
 
 
 
