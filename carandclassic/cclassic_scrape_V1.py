3a3883b8656dbd0ff99ec4ca4a4f611d4f9015f8
yennanliue <f339339@gmail.com>
2018-07-20 18:45:24 +0100

    parse V1 dataframe

--- carandclassic/cclassic_scrape_V1.py
+++ carandclassic/cclassic_scrape_V1.py
@@ -15,8 +15,16 @@ def main():
 	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 	page = opener.open(url)
 	soup = BeautifulSoup(page)
-	soup.find_all('div',attrs={'class': 'item'})[2].find_all('li')
-	print (soup)
+	output={'a':[],'b':[],'c':[],'d':[]}
+	content=soup.find_all('div',attrs={'class': 'item'})
+	for i in range(len(soup.find_all('div',attrs={'class': 'item'}))):
+		#print (content[i].find_all('li')[1].text)
+		output['a'].append(content[i].find_all('li')[0].text)
+		output['b'].append(content[i].find_all('li')[1].text)
+		output['c'].append(content[i].find_all('li')[2].text)
+		output['d'].append(content[i].find_all('li')[3].text)
+	df=pd.DataFrame.from_dict(output)
+	print (df)
 
 
 
