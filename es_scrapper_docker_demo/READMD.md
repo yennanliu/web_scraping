b5cb7d6aa9732892c7cf5ec75e18c623962b6578
yennanliue <f339339@gmail.com>
2019-03-30 08:33:26 +0000

    add scrapper with elasticsearch docker demo

---
+++ es_scrapper_docker_demo/READMD.md
@@ -0,0 +1,12 @@
+### Quick start
+```bash
+#docker build -t es_scrapper_docker_instance . && docker run -it -t es_scrapper_docker_instance
+ 
+docker-compose -f docker-compose.yml up 
+
+```
+
+### Modify from 
+- https://sysadmins.co.za/scraping-websites-with-python-and-beautiful-soup-and-ingesting-into-elasticsearch/
+
+-  https://sysadmins.co.za/building-a-search-engine-for-our-scraped-data-on-elasticsearch-part-2/
---
+++ es_scrapper_docker_demo/app.py
@@ -0,0 +1,45 @@
+import re
+import time
+import requests
+from bs4 import BeautifulSoup
+from elasticsearch import Elasticsearch
+
+es_client = Elasticsearch(['http://localhost:9200'])
+
+drop_index = es_client.indices.create(index='blog-sysadmins', ignore=400)
+create_index = es_client.indices.delete(index='blog-sysadmins', ignore=[400, 404])
+
+def urlparser(title, url):
+    # scrape title
+    p = {}
+    post = title
+    page = requests.get(post).content
+    soup = BeautifulSoup(page, 'lxml')
+    title_name = soup.title.string
+
+    # scrape tags
+    tag_names = []
+    desc = soup.findAll(attrs={"property":"article:tag"})
+    for x in range(len(desc)):
+        tag_names.append(desc[x-1]['content'].encode('utf-8'))
+
+    # payload for elasticsearch
+    doc = {
+        'date': time.strftime("%Y-%m-%d"),
+        'title': title_name,
+        'tags': tag_names,
+        'url': url
+    }
+
+    # ingest payload into elasticsearch
+    res = es_client.index(index="blog-sysadmins", doc_type="docs", body=doc)
+    time.sleep(0.5)
+
+sitemap_feed = 'https://sysadmins.co.za/sitemap-posts.xml'
+page = requests.get(sitemap_feed)
+sitemap_index = BeautifulSoup(page.content, 'html.parser')
+urls = [element.text for element in sitemap_index.findAll('loc')]
+
+for x in urls:
+    print ('x :', x )
+    urlparser(x, x)
