ace50c3a4269f06be6e53fd70c2c5af2fdec9cf7
yennanliue <f339339@gmail.com>
2019-07-19 01:02:55 +0800

    add shell script push to github via travis

---
+++ travis_push_github.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#################################################################
+# MODIFY FROM https://gist.github.com/willprice/e07efd73fb7f13f917ea 
+#################################################################
+
+setup_git() {
+  git config --global user.email $user_email
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+commit_output_file() {
+  git add output/output*.csv
+  git commit --message "Travis build  : $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  echo 'Travis push to github'	
+  git remote add origin-pages https://${GH_TOKEN}@github.com/yennanliu/web_scraping.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+commit_output_file
+upload_files
