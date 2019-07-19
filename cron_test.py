7a7b05b6d673ac6cc5f275eabd53545ee245574d
yennanliue <f339339@gmail.com>
2019-07-20 01:35:57 +0800

    update cron_test.py to save output to txt

--- cron_test.py
+++ cron_test.py
@@ -1,8 +1,14 @@
 import datetime
 
 def main():
-	print('current time : ', datetime.datetime.now())
-	print (123)
+	current_time, current_date  = datetime.datetime.now(), datetime.datetime.now().strftime('%Y-%m-%d')
+	print('current time : ', current_time)
+	with open('output/{}.csv'.format('output-'+str(current_date)), "w") as file:
+		file.write('* this is cron test program \n')
+		file.write(current_date + '\n')
+		file.write('hello world')
+		file.close()
+		print ('write to file OK')
 
 if __name__ == '__main__':
 	main()
