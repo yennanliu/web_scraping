e045626f12121d3e9efabf3ae28151a3e420b51f
Travis CI <f339339@gmail.com>
2019-07-20 02:00:36 +0800

    modify to save output file as txt (csv->txt)

--- cron_test.py
+++ cron_test.py
@@ -3,7 +3,7 @@ import datetime
 def main():
 	current_time, current_date  = datetime.datetime.now(), datetime.datetime.now().strftime('%Y-%m-%d')
 	print('current time : ', current_time)
-	with open('output/{}.csv'.format('output-'+str(current_date)), "w") as file:
+	with open('output/{}.txt'.format('output-'+str(current_date)), "w") as file:
 		file.write('* this is cron test program \n')
 		file.write(str(current_time) + '\n')
 		file.write('hello world')
