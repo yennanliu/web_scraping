8d1d70de99ffd6caf3dc0e78bfc30b1be2e68817
Travis CI <f339339@gmail.com>
2019-07-21 02:08:07 +0800

    add all changes under /output to git and modify the git remote set-url syntac form

--- travis_push_github.sh
+++ travis_push_github.sh
@@ -18,7 +18,7 @@ commit_website_files() {
 
 commit_output_file() {
   git status 
-  git add output/output*.txt
+  git add output/*
   #git add . 
   git commit --m "Travis build  : $TRAVIS_BUILD_NUMBER"
 }
@@ -31,7 +31,7 @@ upload_files() {
   #git push origin master --quiet
 
   #git remote add origin https://${GH_TOKEN}@github.com/yennanliu/web_scraping.git > /dev/null 2>&1
-  echo yes | git remote set-url origin git@github.com:yennanliu/web_scraping.git > /dev/null 2>&1
+  (echo yes | git remote set-url origin git@github.com:yennanliu/web_scraping.git) > /dev/null 2>&1
   echo yes | git push origin master --quiet
 
 }
