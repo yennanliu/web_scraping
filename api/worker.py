e3e8c689e5b7f8935362270208460f92879161e9
Travis CI <f339339@gmail.com>
2019-08-11 17:36:46 +0800

    try to fix library import error by paste path

--- api/worker.py
+++ api/worker.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+import sys 
+sys.path.append("..")
 
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
