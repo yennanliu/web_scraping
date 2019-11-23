f4e820d37936a9f1341965962968b3f2603d109d
yennanliu <f339339@gmail.com>
2019-11-23 21:52:45 +0800

    add test_extract_company

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,13 +1,20 @@
 import sys
 sys.path.append(".")
-from celery_queue.IndeedScrapper.indeed_extract import get_soup as get_soup_
-
+from celery_queue.IndeedScrapper.indeed_extract import (get_soup as get_soup_,
+                                                        extract_company as extract_company_)
 
 def test_get_soup():
     text = """<p>Here's a paragraph of text!</p>"""
     result = get_soup_(text)
     assert result.text.strip() == "Here's a paragraph of text!"
 
+
+def test_extract_company():
+    text = '<div class="row"><a class="result-link-source">google</a></div>'
+    soup = BeautifulSoup(text)
+    result = extract_company_(soup)
+    # todo : add modified html to test extract company method
+    assert result == 'NOT_FOUND'
 
 
 if __name__ == '__main__':
