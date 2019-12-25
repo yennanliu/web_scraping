a7d327a951dad0689307e63b21787f29c5d86ca3
yennanliu <f339339@gmail.com>
2019-12-25 14:46:46 +0800

    add celery unittest

---
+++ tests/unit_test_celery.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+sys.path.append(".")
+from celery_queue.tasks import add, celery
+
+class TestAddTask(unittest.TestCase):
+
+    def setUp(self):
+        self.task = add.apply_async(args=[3, 5])
+        self.results = self.task.get()
+
+    def test_task_state(self):
+        self.assertEqual(self.task.state, "SUCCESS")
+
+    def test_addition(self):
+        self.assertEqual(self.results, 8)
+
+if __name__ == '__main__':
+    unittest.main()
+    pytest.main([__file__])
