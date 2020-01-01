5550f1ed472d3a7048534d5a8c57d18acd5265c7
yennanliu <f339339@gmail.com>
2020-01-01 22:44:29 +0800

    fix code layout (unit_test_celery.py)

--- tests/unit_test_celery.py
+++ tests/unit_test_celery.py
@@ -6,18 +6,15 @@ from unittest.mock import patch
 from unittest import TestCase
 from celery import chain
 from celery_queue import tasks
-from celery_queue.tasks import (celery, 
-                                add, 
-                                multiply, 
-                                scrape)
+# from celery_queue.tasks import (celery, 
+#                                 add, 
+#                                 multiply, 
+#                                 scrape)
 
-"""
-Ref of celery mock unit test
-- https://www.distributedpython.com/2018/05/15/testing-celery-chains/
-- https://www.distributedpython.com/2018/05/01/unit-testing-celery-tasks/
-- http://docs.celeryproject.org/en/latest/userguide/testing.html
-"""
-
+# Ref of celery mock unit test
+# - https://www.distributedpython.com/2018/05/15/testing-celery-chains/
+# - https://www.distributedpython.com/2018/05/01/unit-testing-celery-tasks/
+# - http://docs.celeryproject.org/en/latest/userguide/testing.html
 class TestAddTask(unittest.TestCase):
 
     def test_task_state_and_addition(self):
@@ -41,38 +38,38 @@ class TestScrapeTask(unittest.TestCase):
 
 
 # class TestAddTask(unittest.TestCase):
-
+#
 #     def setUp(self):
 #         self.task = add.apply_async(args=[3, 5])
 #         self.results = self.task.get()
-
+#
 #     def test_task_state(self):
 #         self.assertEqual(self.task.state, "SUCCESS")
-
+#
 #     def test_addition(self):
 #         self.assertEqual(self.results, 8)
-
+#
 # class TestMultiplyTask(unittest.TestCase):
-
+#
 #     def setUp(self):
 #         self.task = multiply.apply_async(args=[3, 5])
 #         self.results = self.task.get()
-
+#
 #     def test_task_state(self):
 #         self.assertEqual(self.task.state, "SUCCESS")
-
+#
 #     def test_multiplication(self):
 #         self.assertEqual(self.results, 15)
-
+#
 # class TestScrapeTask(unittest.TestCase):
-
+#
 #     def setUp(self):
 #         self.task = scrape.apply_async()
 #         self.results = self.task.get()
-
+#
 #     def test_task_state(self):
 #         self.assertEqual(self.task.state, "SUCCESS")
-
+#
 #     def test_scraping(self):
 #         self.assertEqual(type(self.results), str)
 
