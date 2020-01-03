836a784e5a3c24dddc665c44bf6495e2dbba06f9
yennanliu <f339339@gmail.com>
2020-01-03 10:49:04 +0800

    fix func name to make docker-compose build work (api/app.py)

--- api/app.py
+++ api/app.py
@@ -8,25 +8,25 @@ from worker import celery
 app = Flask(__name__)
 
 @app.route('/scrap_task')
-def run_scrap():
+def run_github_scrape():
     task = celery.send_task('tasks.scrap_task',kwargs={})
     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
     return response
 
 @app.route('/scrap_task_api/<string:account>/<string:repo_name>')
-def run_scrap():
+def run_github_scrape_api():
     task = celery.send_task('tasks.scrape_github_api',kwargs={})
     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
     return response
 
 @app.route('/indeed_scrap_task')
-def run_indeed_scrap():
+def run_indeed_scrape():
     task = celery.send_task('tasks.indeed_scrap_task',kwargs={})
     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
     return response
 
 @app.route('/indeed_scrap_api_V1/<string:city_set>')
-def run_indeed_scrap_api(city_set: str):
+def run_indeed_scrape_api(city_set: str):
     print ('city_set :', city_set)
     task = celery.send_task('tasks.indeed_scrap_api_V1',city_set,kwargs={})
     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
